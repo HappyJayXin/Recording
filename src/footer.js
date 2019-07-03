7b1b30a08abe1519e43c0e555089523c48543c4e
HappyJayXin <36494560+HappyJayXin@users.noreply.github.com>
2019-07-03 13:34:47 +0800

    :lipstick: header and footer style change

--- src/footer.js
+++ src/footer.js
@@ -2,7 +2,7 @@ Vue.component('footer-component', {
   template: `
     <footer class="bg-light">
         <div class="text-center container-fluid">
-            <h5>2019 &copy;XinGyu/Jay</h5>
+            <h5 class="mb-0">2019 &copy; Happyjayxin / Jay</h5>
         </div>
     </footer>
     `
--- src/header.js
+++ src/header.js
@@ -1,15 +1,15 @@
 Vue.component('header-component', {
   template: `
-    <nav class="navbar navbar-light bg-light">
-        <h1>紀錄時間</h1>
-        <button
-          type="button"
-          class="btn btn-dark"
-          data-toggle="modal"
-          data-target="#newDataModal"
-        >
-          新增
-        </button>
-    </nav>
+  <nav class="navbar navbar-light bg-light">
+    <h1 class="mb-0">紀錄時間</h1>
+    <button
+      type="button"
+      class="btn btn-dark"
+      data-toggle="modal"
+      data-target="#newDataModal"
+    >
+      新增
+    </button>
+  </nav>
   `
 })
