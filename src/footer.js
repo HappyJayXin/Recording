9d554b25c8f112b1624e51bc6e23577ba075e3ad
Jay <4a490063@stust.edu.tw>
2020-10-23 15:24:04 +0800

    add year in footer

--- src/footer.js
+++ src/footer.js
@@ -1,9 +1,14 @@
 Vue.component('footer-component', {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  },
   template: `
     <footer class="bg-light">
         <div class="text-center container-fluid">
-            <h5 class="mb-0">2019 &copy; Happyjayxin / Jay</h5>
+            <h5 class="mb-0">2019 - {{ year }} &copy; Jay</h5>
         </div>
     </footer>
-    `
-})
+    `,
+});
