3f4d0d31a1aa3517663f249a9a5c1b643770ffb0
Jay <4a490063@stust.edu.tw>
2020-10-23 15:18:06 +0800

    update makeHour to 5

--- src/script.js
+++ src/script.js
@@ -4,13 +4,13 @@ let data = {
   buttonState: {
     edit: false,
     add: true,
-    index: ''
+    index: '',
   },
   fromData: {
     episode: [],
     hour: [],
     minute: [],
-    error: false
+    error: false,
   },
   input: {
     id: 0,
@@ -18,93 +18,93 @@ let data = {
     episode: 0,
     hour: 0,
     minute: 0,
-    url: ''
+    url: '',
   },
-  contents: []
-}
+  contents: [],
+};
 
 let vm = new Vue({
   el: '#app',
   data() {
-    return data
+    return data;
   },
   methods: {
     handleAdd() {
-      const { name, episode, hour, minute } = this.input
+      const { name, episode, hour, minute } = this.input;
       if (name !== '' && episode !== '' && hour !== null && minute !== null) {
-        this.fromData.error = false
+        this.fromData.error = false;
         // make id to li element
-        this.input.id = Math.floor(Math.random() * 100000)
-        this.contents = this.contents || []
-        this.contents.push(this.input)
+        this.input.id = Math.floor(Math.random() * 100000);
+        this.contents = this.contents || [];
+        this.contents.push(this.input);
 
-        $('#newDataModal').modal('hide')
-        this.clearInput()
-        this.saveDataToLocalStorage()
+        $('#newDataModal').modal('hide');
+        this.clearInput();
+        this.saveDataToLocalStorage();
       } else {
-        this.fromData.error = true
+        this.fromData.error = true;
       }
       $('#newDataModal').on('hide.bs.modal', () => {
-        this.fromData.error = false
-      })
+        this.fromData.error = false;
+      });
     },
     openEditView(i) {
       // Show only the edit button
       this.buttonState = {
         edit: true,
         add: false,
-        index: i
-      }
-      const { id, name, episode, hour, minute, url } = this.contents[i]
+        index: i,
+      };
+      const { id, name, episode, hour, minute, url } = this.contents[i];
       this.input = {
-        id: id,
-        name: name,
-        episode: episode,
-        hour: hour,
-        minute: minute,
-        url: url
-      }
-      this.clearForm()
+        id,
+        name,
+        episode,
+        hour,
+        minute,
+        url,
+      };
+      this.clearForm();
       $('#newDataModal').on('hide.bs.modal', () => {
-        this.buttonState.edit = false
-        this.buttonState.add = true
-      })
+        this.buttonState.edit = false;
+        this.buttonState.add = true;
+      });
     },
     handleEdit() {
-      const { name, episode, hour, minute } = this.input
+      const { name, episode, hour, minute } = this.input;
       if (name !== '' && episode !== '' && hour !== null && minute !== null) {
-        this.fromData.error = false
+        this.fromData.error = false;
         // Show only add buttons
-        this.buttonState.edit = false
-        this.buttonState.add = true
-        this.contents[this.buttonState.index] = this.input
+        this.buttonState.edit = false;
+        this.buttonState.add = true;
+        this.contents[this.buttonState.index] = this.input;
 
-        this.clearInput()
-        $('#newDataModal').modal('hide')
-        this.saveDataToLocalStorage()
+        this.clearInput();
+        $('#newDataModal').modal('hide');
+        this.saveDataToLocalStorage();
       } else {
-        this.fromData.error = true
+        this.fromData.error = true;
       }
     },
     handleDelete(i) {
-      if (window.confirm('確定刪除嗎?')) this.contents.splice(i, 1)
-      this.saveDataToLocalStorage()
+      if (window.confirm('確定刪除嗎?')) this.contents.splice(i, 1);
+      this.saveDataToLocalStorage();
     },
     handleClear() {
-      this.clearInput()
+      this.clearInput();
     },
     // Open new window
     handleOpenVideo(i) {
-      const url = this.contents[i].url
-      if (!url) return
-      window.open(url)
+      const url = this.contents[i].url;
+      if (!url) return;
+      window.open(url);
     },
     clearForm() {
       // when leave clear input data
       $('#newDataModal').on('hide.bs.modal', () => {
-        this.fromData.error = false
-        this.clearInput()
-      })
+        this.fromData.error = false;
+        this.clearInput();
+      });
     },
     clearInput() {
       this.input = {
@@ -112,39 +112,39 @@ let vm = new Vue({
         episode: 0,
         hour: 0,
         minute: 0,
-        url: ''
-      }
+        url: '',
+      };
     },
     // If have set Url show underLine
     haveVideo(i) {
       return {
-        underLine: this.contents[i].url
-      }
+        underLine: this.contents[i].url,
+      };
     },
     // Save data to LocalStorage
     saveDataToLocalStorage() {
-      localStorage.setItem('RecordData', JSON.stringify(this.contents))
-    }
+      localStorage.setItem('RecordData', JSON.stringify(this.contents));
+    },
   },
   computed: {
     // put data to select option
     makeHour() {
-      for (let i = 0; i <= 3; i++) {
-        this.fromData.hour.push(i)
+      for (let i = 0; i <= 5; i++) {
+        this.fromData.hour.push(i);
       }
-      return this.fromData.hour
+      return this.fromData.hour;
     },
     makeMinute() {
       for (let i = 0; i <= 59; i++) {
-        this.fromData.minute.push(i)
+        this.fromData.minute.push(i);
       }
-      return this.fromData.minute
-    }
+      return this.fromData.minute;
+    },
   },
   mounted() {
     // Get localStorage data
     if (localStorage.getItem('RecordData')) {
-      this.contents = JSON.parse(localStorage.getItem('RecordData'))
+      this.contents = JSON.parse(localStorage.getItem('RecordData'));
     }
-  }
-})
+  },
+});
